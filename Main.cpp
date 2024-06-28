31a6f33c1f4315239f498d1b39c8118e40571d05
Jose <Jose@DESKTOP-8BHCOHK>
2024-06-27 21:09:50 -0700

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,40 @@
+//Hello Project is a Project design to be collaborative among users and the software program 
+//Goal of the project: Happy Birthday "Insert name" for any birthday surpirses  coming up 
+//6/27/24 
+
+
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main() {
+
+    string name;
+    cout << "Enter Your Name Please" << endl;
+
+    cin >> name;
+
+
+    for (int a = 0.5; a < 18; a++) {
+
+        cout << "Happy Birthday!";
+    }
+
+    for (int a = 0.5; a < 200; a++) {
+
+        cout << "<3";
+
+    }
+
+    cout << endl;
+
+
+    cout << "May God Bless You with many Years! ";
+
+    cout << "Enjoy Your Day!";
+
+
+    return 0;
+
+}
